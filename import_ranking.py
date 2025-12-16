32e8c1416b1aa643b9dcc307e023c9a8ffdd04ab
Jimmy Nguyen <jtngyen5@memphis.edu>
2025-12-16 17:31:52 -0600

    Added the dataframes for the spreadsheets of the countries, years, and club 1769, as well as the list of users participating.

--- import_ranking.py
+++ import_ranking.py
@@ -1,3 +1,7 @@
+"""
+This Python file contains the ranking dataframe and the list of countries.
+"""
+
 # Imports pandas to the file.
 import pandas as pd
 
@@ -10,10 +14,33 @@ pd.set_option('display.max_colwidth', None)
 
 # Imports the ranking csv file and converts it to be printed out.
 ranking_df = pd.read_csv("ranking.csv").values.tolist()
+# Imports the ranking csv file and converts it to be printed out.
+countries_df = pd.read_csv("countries.csv").values.tolist()
+# Imports the ranking csv file and converts it to be printed out.
+years_df = pd.read_csv("years.csv").values.tolist()
+# Imports the ranking csv file and converts it to be printed out.
+ranking_1769_df = pd.read_csv("club_1769.csv").values.tolist()
+# List of countries is empty, but will be appended.
 countries = []
+# List of the users who ranked all the songs.
+users_1769 = {}
+# List of the users who didn't rank all the songs.
+all_users = {}
 
+# Adds all the countries to the list of countries.
 for row in ranking_df:
     if row[7] not in countries:
         countries.append(row[7])
-
+# Removes nan from the list, which is the first element.
 countries.pop(0)
+
+# Appends the dictionary containing the participating users and how many entries they ranked.
+for k in range(17, len(ranking_1769_df[1])):
+    user = ranking_1769_df[1][k]
+    users_1769[user] = 1769
+
+for m in range(17, len(ranking_df[1])):
+    if ranking_df[0][m] == "1769":
+        continue
+    user2 = ranking_df[1][m]
+    all_users[user2] = ranking_df[0][m]
--- main.py
+++ main.py
@@ -1,11 +1,38 @@
-from import_ranking import ranking_df, countries
+"""
+This is the main program where the user will analyze the ranking sheet.
+"""
 
+# Imports the ranking and list of countries to this program.
+from import_ranking import ranking_df, countries_df, years_df, ranking_1769_df, countries, users_1769, all_users
+
+
+# Main function containing the main program.
 def main():
     # for i in range(4, 104):
     #     print(ranking_df[i])
     # print(countries)
     # print(len(countries))
-    print("Welcome to the Analyzer250")
-
+    print("Welcome to the Analyzer250!\n\n")
+    print("What would you like to get from the ranking of ALTESC250 2024?")
+    print("1. List of the entire ranking.")
+    print(users_1769)
+    print(all_users)
+    # 1. List of the entire ranking. (ranking.csv)
+    # 2. List of the entire ranking from those who ranked all the songs (club_1769.csv).
+    # 3. List of the entire ranking from a specific user (ranking.csv).
+    # 4. All years ranked by average.
+    # 5. All years ranked by average from those who ranked all the songs.
+    # 6. All years ranked by average of a specific user.
+    # 7. All the countries ranked by average.
+    # 8. All the countries ranked by average from those who ranked all the songs.
+    # 9. All the countries ranked by average of a specific user.
+    # 10. All the entries from a specific year ranked.
+    # 11. All the entries from a specific year ranked from those who ranked all the songs.
+    # 12. All the entries from a specific year ranked from a specific user.
+    # 13. All the entries from a specific country ranked.
+    # 14. All the entries from a specific country ranked from those who ranked all the songs.
+    # 15. All the entries from a specific country ranked from a specific user.
+    
+# This runs the main program.
 if __name__ == "__main__":
     main()
