e65a5781cd352cd6f150e7b8f2e3a352c8b0c1d5
Jimmy Nguyen <jtngyen5@memphis.edu>
2025-12-16 21:48:58 -0600

    Added user input for their choice and functions that print out the entire ranking and each year's average based on all the users.

--- main.py
+++ main.py
@@ -5,6 +5,17 @@ This is the main program where the user will analyze the ranking sheet.
 # Imports the ranking and list of countries to this program.
 from import_ranking import ranking_df, countries_df, years_df, ranking_1769_df, countries, users_1769, all_users
 
+def entire_ranking(ranking=ranking_df):
+    print("The Entire Ranking (All Users)")
+    for n in range(4, len(ranking)):
+        entry = ranking[n]
+        print(f"{entry[0]}. {entry[7]} {entry[3]}: {entry[10]} - {entry[11]} - {entry[14]}")
+
+def all_year_average(ranking=years_df):
+    print("All Years Ranked by Averages")
+    for n in range(len(ranking)):
+        year = ranking[n]
+        print(f"{year}")
 
 # Main function containing the main program.
 def main():
@@ -12,11 +23,28 @@ def main():
     #     print(ranking_df[i])
     # print(countries)
     # print(len(countries))
+    # Print statements
     print("Welcome to the Analyzer250!\n\n")
-    print("What would you like to get from the ranking of ALTESC250 2024?")
-    print("1. List of the entire ranking.")
-    print(users_1769)
-    print(all_users)
+    print("What would you like to get from the ranking of ALTESC250 2024?\n")
+    # Choices
+    print("1 - List of the entire ranking.")
+    print("2 - List of the entire ranking.")
+    # Gets user input
+    choice1 = input("Type in the number associated with the choice you want: ")
+    # Ensures that the user enters the number listed in the choices
+    while choice1 not in [str(c) for c in range(1, 15)]:
+        print("That is not a number listed. Try a number listed!")
+        print("Here are the choices again.\n")
+        choice1 = input("Type in the number associated with the choice you want: ")
+    # Else statement converts the choice into an int value.
+    else:
+        choice1 = int(choice1)
+    # If statements that calls each function depending on the choice.
+    if choice1 == 1:
+        entire_ranking()
+    elif choice1 == 2:
+        all_year_average()
+
     # 1. List of the entire ranking. (ranking.csv)
     # 2. List of the entire ranking from those who ranked all the songs (club_1769.csv).
     # 3. List of the entire ranking from a specific user (ranking.csv).
