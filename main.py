df9f92838f46859498e0dbb8285fa45d9745b0b1
Jimmy Nguyen <jtngyen5@memphis.edu>
2025-12-16 22:42:36 -0600

    Updated the function that prints out the ranking of the years by average of all the users.

--- main.py
+++ main.py
@@ -5,30 +5,46 @@ This is the main program where the user will analyze the ranking sheet.
 # Imports the ranking and list of countries to this program.
 from import_ranking import ranking_df, countries_df, years_df, ranking_1769_df, countries, users_1769, all_users
 
+# Function prints out the entire ranking.
 def entire_ranking(ranking=ranking_df):
     print("The Entire Ranking (All Users)")
     for n in range(4, len(ranking)):
         entry = ranking[n]
         print(f"{entry[0]}. {entry[7]} {entry[3]}: {entry[10]} - {entry[11]} - {entry[14]}")
 
+# Function prints out the averages 
 def all_year_average(ranking=years_df):
     print("All Years Ranked by Averages")
     for n in range(len(ranking)):
         year = ranking[n]
-        print(f"{year}")
+        print(f"{year[0]}. {year[1]} - {year[2]}")
 
 # Main function containing the main program.
 def main():
-    # for i in range(4, 104):
-    #     print(ranking_df[i])
-    # print(countries)
-    # print(len(countries))
     # Print statements
     print("Welcome to the Analyzer250!\n\n")
     print("What would you like to get from the ranking of ALTESC250 2024?\n")
     # Choices
+    # 1. List of the entire ranking. (ranking.csv)
+    # 2. List of the entire ranking from those who ranked all the songs (club_1769.csv).
+    # 3. List of the entire ranking from a specific user (ranking.csv).
     print("1 - List of the entire ranking.")
-    print("2 - List of the entire ranking.")
+    # 4. All years ranked by average.
+    # 5. All years ranked by average from those who ranked all the songs.
+    # 6. All years ranked by average of a specific user.
+    print("2 - All the years ranked by average.")
+    # 7. All the countries ranked by average.
+    # 8. All the countries ranked by average from those who ranked all the songs.
+    # 9. All the countries ranked by average of a specific user.
+    print("3 - All the countries ranked by average.")
+    # 10. All the entries from a specific year ranked.
+    # 11. All the entries from a specific year ranked from those who ranked all the songs.
+    # 12. All the entries from a specific year ranked from a specific user.
+    print("4 - All the entries from a specific year ranked.")
+    # 13. All the entries from a specific country ranked.
+    # 14. All the entries from a specific country ranked from those who ranked all the songs.
+    # 15. All the entries from a specific country ranked from a specific user.
+    print("5 - All the entries from a specific country ranked.")
     # Gets user input
     choice1 = input("Type in the number associated with the choice you want: ")
     # Ensures that the user enters the number listed in the choices
@@ -44,22 +60,6 @@ def main():
         entire_ranking()
     elif choice1 == 2:
         all_year_average()
-
-    # 1. List of the entire ranking. (ranking.csv)
-    # 2. List of the entire ranking from those who ranked all the songs (club_1769.csv).
-    # 3. List of the entire ranking from a specific user (ranking.csv).
-    # 4. All years ranked by average.
-    # 5. All years ranked by average from those who ranked all the songs.
-    # 6. All years ranked by average of a specific user.
-    # 7. All the countries ranked by average.
-    # 8. All the countries ranked by average from those who ranked all the songs.
-    # 9. All the countries ranked by average of a specific user.
-    # 10. All the entries from a specific year ranked.
-    # 11. All the entries from a specific year ranked from those who ranked all the songs.
-    # 12. All the entries from a specific year ranked from a specific user.
-    # 13. All the entries from a specific country ranked.
-    # 14. All the entries from a specific country ranked from those who ranked all the songs.
-    # 15. All the entries from a specific country ranked from a specific user.
     
 # This runs the main program.
 if __name__ == "__main__":
