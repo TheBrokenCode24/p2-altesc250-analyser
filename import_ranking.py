5756efff0c8a57284c47124beb3767ebfb9cc55d
Jimmy Nguyen <jtngyen5@memphis.edu>
2025-12-30 12:07:01 -0600

    Added the CSV files for both the 2023 and 2025 editions and changed the variables to reflect which year they are getting the csv files from.

--- import_ranking.py
+++ import_ranking.py
@@ -13,26 +13,26 @@ pd.set_option('display.max_columns', None)
 pd.set_option('display.max_colwidth', None)
 
 # Imports the ranking csv file and converts it to be printed out.
-ranking_df = pd.read_csv("ranking.csv").values.tolist()
+ranking_2024 = pd.read_csv("csv_files/2024/ranking.csv").values.tolist()
 # Imports the ranking csv file and converts it to be printed out.
-countries_df = pd.read_csv("countries.csv").values.tolist()
+countries_df = pd.read_csv("csv_files/2024/countries.csv").values.tolist()
 # Imports the ranking csv file and converts it to be printed out.
-years_df = pd.read_csv("years.csv").values.tolist()
+years_df = pd.read_csv("csv_files/2024/years.csv").values.tolist()
 # Imports the ranking csv file and converts it to be printed out.
-ranking_1769_df = pd.read_csv("club_1769.csv").values.tolist()
+ranking_1769_df = pd.read_csv("csv_files/2024/club1769.csv").values.tolist()
 # List of countries is empty, but will be appended.
 countries = []
 # List of all the users.
 users = {}
 
 # Adds all the countries to the list of countries.
-for row in ranking_df:
+for row in ranking_2024:
     if row[7] not in countries:
         countries.append(row[7])
 # Removes nan from the list, which is the first element.
 countries.pop(0)
 
 # Appends the dictionary containing the participating users and how many entries they ranked.
-for k in range(17, len(ranking_df[1])):
-    us = ranking_df[1][k]
-    users[us] = int(ranking_df[0][k])
+for k in range(17, len(ranking_2024[1])):
+    us = ranking_2024[1][k]
+    users[us] = int(ranking_2024[0][k])
